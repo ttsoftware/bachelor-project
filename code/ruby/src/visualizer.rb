2bd87279c24435a23d6ed200e56b11579ec823c4
RasmusHaarslev <rasmus.haarslev@gmail.com>
2015-05-25 15:04:13 +0200

    code

--- code/ruby/src/visualizer.rb
+++ code/ruby/src/visualizer.rb
@@ -9,7 +9,7 @@ class Visualizer
         IO.popen('gnuplot', 'w') { |io| io.puts commands }
     end
 
-    def visualzie
+    def visualize
         commands = '
             set terminal svg
             # white background
@@ -18,6 +18,16 @@ class Visualizer
             # file name
             set output "penis.svg"
             # data
+            set style line 1 lc rgb "#FF6060" lt 1 lw 2 pt 7 ps 0.5   # --- red
+            set style line 2 lc rgb "#6060FF" lt 1 lw 2 pt 7 ps 0.5   # --- blue
+            set style line 3 lc rgb "#60FF60" lt 1 lw 2 pt 7 ps 0.5   # --- green
+
+
+            set xr [0:10]
+            set yr [0:100]
+            plot "../patscan-patterns/benchmark/results/result_data/re2_deletions.data" with linespoints ls 1 title "RE2", \
+                 "../patscan-patterns/benchmark/results/result_data/ruby_deletions.data" with linespoints ls 2 title "Ruby", \
+                 "../patscan-patterns/benchmark/results/result_data/scan_for_matches_deletions.data" with linespoints ls 3 title "scan-for-matches"
         '
 
         gnuplot(commands)
