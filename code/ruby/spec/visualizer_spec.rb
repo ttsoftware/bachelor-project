18d12a09fc9b31ee39e3ec3e452b6856805f163e
RasmusHaarslev <rasmus.haarslev@gmail.com>
2015-05-25 16:17:28 +0200

    code

--- code/ruby/spec/visualizer_spec.rb
+++ code/ruby/spec/visualizer_spec.rb
@@ -7,7 +7,12 @@ describe Visualizer, :type => :class do
         it 'should visualize json data' do
 
             visualizer = Visualizer.new
-            visualizer.visualize
+            visualizer.visualize :mismatches
+            visualizer.visualize :deletions
+            visualizer.visualize :insertions
+            visualizer.visualize :combinations
+            visualizer.visualize :ranges
+            visualizer.visualize :sequences
         end
     end
 end
--- code/ruby/src/visualizer.rb
+++ code/ruby/src/visualizer.rb
@@ -2,37 +2,87 @@ class Visualizer
 
     def initialize
 
-        @re2_deletions_data = '../patscan-patterns/benchmark/results/result_data/re2_deletions.data'
-        @ruby_deletions_data = '../patscan-patterns/benchmark/results/result_data/ruby_deletions.data'
-        @scan_for_matches_deletions_data = '../patscan-patterns/benchmark/results/result_data/scan_for_matches_deletions.data'
+        @mode = {
+            :mismatches => {
+                :re2 => '../patscan-patterns/benchmark/results/result_data/re2_mismatches.data',
+                :ruby => '../patscan-patterns/benchmark/results/result_data/ruby_mismatches.data',
+                :scan => '../patscan-patterns/benchmark/results/result_data/scan_for_matches_mismatches.data',
+                :xlabel => 'Patscan complexity',
+                :ylabel => 'Match time [ms]',
+                :title => 'Mismatches: Patscan Complexity'
+            },
+            :deletions => {
+                :re2 => '../patscan-patterns/benchmark/results/result_data/re2_deletions.data',
+                :ruby => '../patscan-patterns/benchmark/results/result_data/ruby_deletions.data',
+                :scan => '../patscan-patterns/benchmark/results/result_data/scan_for_matches_deletions.data',
+                :xlabel => 'Patscan complexity',
+                :ylabel => 'Match time [ms]',
+                :title => 'Deletions: Patscan Complexity'
+            },
+            :insertions => {
+                :re2 => '../patscan-patterns/benchmark/results/result_data/re2_insertions.data',
+                :ruby => '../patscan-patterns/benchmark/results/result_data/ruby_insertions.data',
+                :scan => '../patscan-patterns/benchmark/results/result_data/scan_for_matches_insertions.data',
+                :xlabel => 'Patscan complexity',
+                :ylabel => 'Match time [ms]',
+                :title => 'Insertions: Patscan Complexity'
+            },
+            :combinations => {
+                :re2 => '../patscan-patterns/benchmark/results/result_data/re2_combinations.data',
+                :ruby => '../patscan-patterns/benchmark/results/result_data/ruby_combinations.data',
+                :scan => '../patscan-patterns/benchmark/results/result_data/scan_for_matches_combinations.data',
+                :xlabel => 'Patscan complexity',
+                :ylabel => 'Match time [ms]',
+                :title => 'Combinations: Patscan Complexity'
+            },
+            :ranges => {
+                :re2 => '../patscan-patterns/benchmark/results/result_data/re2_range.data',
+                :ruby => '../patscan-patterns/benchmark/results/result_data/ruby_range.data',
+                :scan => '../patscan-patterns/benchmark/results/result_data/scan_for_matches_range.data',
+                :xlabel => 'Patscan complexity',
+                :ylabel => 'Match time [ms]',
+                :title => 'Ranges: Patscan Complexity'
+            },
+            :sequences => {
+                :re2 => '../patscan-patterns/benchmark/results/result_data/re2_sequences.data',
+                :ruby => '../patscan-patterns/benchmark/results/result_data/ruby_sequences.data',
+                :scan => '../patscan-patterns/benchmark/results/result_data/scan_for_matches_sequences.data',
+                :xlabel => 'Patscan complexity',
+                :ylabel => 'Match time [ms]',
+                :title => 'Sequences: Patscan Complexity'
+            }
+        }
     end
 
     def gnuplot(commands)
         IO.popen('gnuplot', 'w') { |io| io.puts commands }
     end
 
-    def visualize
+    def visualize(mode)
         commands = "
             set terminal svg
-
             # white background
             set object 1 rect from screen 0, 0, 0 to screen 1, 1, 0 behind
             set object 1 rect fc  rgb 'white'  fillstyle solid 1.0
 
             # file name
-            set output 'penis.svg'
+            set output '#{mode}.svg'
+
+            # axes label
+            set xlabel '#{@mode[mode][:xlabel]}'
+            set ylabel '#{@mode[mode][:ylabel]}'
+
+            # title
+            set title '#{@mode[mode][:title]}'
 
             # data
             set style line 1 lc rgb '#FF6060' lt 1 lw 2 pt 7 ps 0.5   # --- red
             set style line 2 lc rgb '#6060FF' lt 1 lw 2 pt 7 ps 0.5   # --- blue
             set style line 3 lc rgb '#60FF60' lt 1 lw 2 pt 7 ps 0.5   # --- green
 
-            #set xr [0:10]
-            #set yr [0:100]
-
-            plot '#{@re2_deletions_data}' with linespoints ls 1 title 'RE2', \
-                 '#{@ruby_deletions_data}' with linespoints ls 2 title 'Ruby', \
-                 '#{@scan_for_matches_deletions_data}' with linespoints ls 3 title 'scan_for_matches'
+            plot '#{@mode[mode][:re2]}' with linespoints ls 1 title 'RE2', \
+                 '#{@mode[mode][:ruby]}' with linespoints ls 2 title 'Ruby', \
+                 '#{@mode[mode][:scan]}' with linespoints ls 3 title 'scan_for_matches'
         "
 
         gnuplot(commands)
